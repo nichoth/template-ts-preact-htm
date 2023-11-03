9b380934a26ee15d6a38a6bd29a8d91a92f2f5a3
nick thomas <nichoth@gmail.com>
2023-11-03 13:31:59 -0700

    add debug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { html } from 'htm/preact'
+import { createDebug } from '@nichoth/debug'
+
+const debug = createDebug()
 
 export function Example () {
+    debug('rendering...')
     return html`<div>hello</div>`
 }
